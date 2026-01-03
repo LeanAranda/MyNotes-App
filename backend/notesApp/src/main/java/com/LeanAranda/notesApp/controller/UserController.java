2c0f575a014cf6afb987ac191eb997a47fc1d540
LeanAranda <arandaleandro2003@gmail.com>
2026-01-03 13:29:40 -0300

    backend/add cors permission

--- backend/notesApp/src/main/java/com/LeanAranda/notesApp/controller/UserController.java
+++ backend/notesApp/src/main/java/com/LeanAranda/notesApp/controller/UserController.java
@@ -4,10 +4,7 @@ import com.LeanAranda.notesApp.dto.UserDto;
 import com.LeanAranda.notesApp.mapper.UserMapper;
 import com.LeanAranda.notesApp.model.User;
 import com.LeanAranda.notesApp.service.IUserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")

0013ad9100bd643ed479f308a8657069e7b89105
LeanAranda <arandaleandro2003@gmail.com>
2026-01-04 00:58:04 -0300

    frontend/add switch color mode function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,31 @@
 import { useState } from 'react'
 import LoginForm from './components/LoginForm.jsx'
 import NotesList from "./components/NotesList";
+import colorModeIcon from "./assets/color-mode-white.svg";
 
 export default function App() {
   // state to track if the user is logged in
   const [logged, setLogged] = useState(!!localStorage.getItem("token"));
   const [view, setView] = useState("active")
+  const [isYellowMode, setIsYellowMode] = useState(true);
 
   function handleLogout() {
     localStorage.removeItem("token");
     setLogged(false);
   }
 
+  const toggleMode = () => { setIsYellowMode(!isYellowMode); };
+
   return (
-    <div className="page-container">
+    <div className="page-container" style={{ backgroundColor: isYellowMode ? "#fde991" : "#ebebeb" }}>
       <header className="navbar">
         <h2>My Notes</h2>
-        {logged && <button onClick={handleLogout}>Logout</button>}
+        <div className='nav-buttons'>
+          <button onClick={toggleMode}>
+            <img src={colorModeIcon} width={32} height={32} alt="Toggle Color Mode" />
+          </button>
+          {logged && <button onClick={handleLogout}>Logout</button>}
+        </div>
       </header>
         {logged ? (
           <>
